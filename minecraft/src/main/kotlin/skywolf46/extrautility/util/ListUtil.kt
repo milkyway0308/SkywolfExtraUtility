04de1f6411d4953497f1f8769da25c9859cf9db5
milkyway0308 <skywolf461@gmail.com>
2022-02-14 03:25:05 +0900

    1.74.0 - Added percentage based list sampling

--- minecraft/src/main/kotlin/skywolf46/extrautility/util/ListUtil.kt
+++ minecraft/src/main/kotlin/skywolf46/extrautility/util/ListUtil.kt
@@ -7,3 +7,7 @@ fun <T : Any> List<T>.sample(amount: Int): List<T> {
         (0 until amount.coerceAtMost(size)).map { _ -> removeAt(Random.nextInt(size)) }
     }
 }
+
+fun <T : Any> List<T>.samplePercentage(percentage: Double): List<T> {
+    return sample((size * (percentage.coerceAtLeast(0.0).coerceAtMost(100.0) * 0.01)).toInt())
+}
